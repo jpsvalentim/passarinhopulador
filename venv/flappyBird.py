8ad4a095d89cfc00f99192913e3d2a81551c514b
João Pedro Silva Valentim <j.pedro__@hotmail.com>
2025-04-05 20:33:50 -0300

    finalização 2

--- venv/flappyBird.py
+++ venv/flappyBird.py
@@ -196,7 +196,7 @@ def desenhar_tela(tela, passaros, canos, chao, pontos):
 
 def main():
     passaros = [Passaro(230, 350)]
-    Chao = Chao(730)
+    chao = Chao(730)
     canos = [Cano(700)]
     tela = pygame.display.set_mode((TELA_LARGURA,TELA_ALTURA))
     pontos = 0
@@ -222,16 +222,35 @@ def main():
         chao.mover()
 
         adicionar_cano = False
+        remover_canos =[]
         for cano in canos:
             for i, passaro in enumerate(passaros):
-                if cano.colidir(passaros):
-                    passaros(i)
-                    
+                if cano.colidir(passaro):
+                    passaros.pop(i)
 
+                if not cano.passou and passaro.x  > cano.x:
+                    cano.passou = True
+                    adicionar_cano = True
+            cano .mover()
+            if cano.x  + cano.CANO_TOPO.get_width() <0:
+                remover_canos.append(cano)
+
+        if adicionar_cano:
+            pontos +=1
+            canos.append(Cano(600))
+        for cano in remover_canos:
+            canos.remove(cano)
+        
+        for i, passaro in enumerate(passaros):
+            if(passaro.y +passaro.imagem.get_height())> chao.y or passaro.y <0:
+                passaros.pop(i)
 
 
         desenhar_tela(tela, passaros, canos, chao, pontos)
 
+
+if __name__ == "__main__":
+    main()
 
 
 
